8c2b141f4e7e0b6d6b2a13b1197d416af759d1ff
Andrew Robinson <andrew_robinson_1977@yahoo.co.uk>
2019-04-02 19:19:03 -0400

    Bump version to 0.0.2+dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TkinterExtensionPack",
-    version="0.0.2",
+    version="0.0.2+dev",
     author="Andrew Robinson",
     author_email="flabby1976@gmail.com",
     description="Useful tkinter extensions",
